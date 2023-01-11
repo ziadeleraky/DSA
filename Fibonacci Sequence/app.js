ee859c269e68448a50e723b1a95feb6fe1931049
Ziad Eleraky <ziadeleraky1@gmail.com>
2023-01-11 20:09:33 +0200

    Code Improved

--- Fibonacci Sequence/app.js
+++ Fibonacci Sequence/app.js
@@ -1,18 +1,14 @@
-let arr = [0, 1];
-
 function Fibonacci(num) {
-  if (num < 3) {
-    return arr;
-  } else {
-    for (let i = 0; i < num; i++) {
-      console.log(i);
-      arr.push(arr[i] + arr[i + 1]);
-      if (arr.length == num) {
-        break;
-      }
-    }
-    return arr;
+  let arr = [0, 1];
+  // Won't execute the loop if num < 3, because of the intial value of i
+  for (let i = 2; i < num; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
   }
+  return arr;
 }
 
-console.log(Fibonacci(8));
+console.log(Fibonacci(0));
+console.log(Fibonacci(3));
+console.log(Fibonacci(5));
+
+// Big-O (Time Complexity) = O(n)
