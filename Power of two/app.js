f5d9e397a7c7a30d81b2e3d6a493e23351d74fbe
Ziad Eleraky <ziadeleraky1@gmail.com>
2023-01-11 22:20:31 +0200

    O(logn) Solution

--- Power of two/app.js
+++ Power of two/app.js
@@ -1,13 +1,18 @@
 function isPowerOfTwo(num) {
-  let powerOfTwo = 1;
-  for (let i = 0; i < num; i++) {
-    if (powerOfTwo == num) {
-      return true;
+  if (num < 1) {
+    return false;
+  }
+
+  while (num > 1) {
+    // If the current number is an even number then divide it by 2 till it becomes 2/2 = 1
+    if (num % 2 === 0) {
+      num /= 2;
     } else {
-      powerOfTwo *= 2;
+      return false;
     }
   }
-  return false;
+
+  return true;
 }
 
 console.log(isPowerOfTwo(1));
@@ -15,5 +20,7 @@ console.log(isPowerOfTwo(0));
 console.log(isPowerOfTwo(4));
 console.log(isPowerOfTwo(8));
 console.log(isPowerOfTwo(11));
+console.log(isPowerOfTwo(6));
 
-// Big-O = O(n)
+// Input size is reduced by half
+// Big-O = O(logn)
