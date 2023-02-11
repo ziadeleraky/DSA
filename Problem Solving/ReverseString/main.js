70ee1c21418b28e9ea5e4e550a69b3c108a52c54
Ziad Eleraky <ziadeleraky1@gmail.com>
2023-02-12 00:26:07 +0200

    Spread Operator

--- Problem Solving/ReverseString/main.js
+++ Problem Solving/ReverseString/main.js
@@ -1,4 +1,3 @@
-// function to reverse a string
 function reverseString(str) {
   let rev = [];
   let count = 0;
@@ -9,19 +8,31 @@ function reverseString(str) {
   return rev.join("");
 }
 
-//? time complexity O(n) because we loop through the string once
-//? space complexity O(n) because we use an array to store the reversed string
+// time complexity O(n) because we loop through the string once
+// space complexity O(n) because we use an array to store the reversed string
 
-//! test
-console.log(reverseString("ziad ahmed eleraky"));
+// test
+console.log("Rev1:", reverseString("ziad ahmed eleraky"));
 
 // ------------------------------
 
 function revString(str) {
   return str.split("").reverse().join("");
 }
-// time complexity O(n) because we loop through the string once
-// space complexity O(n) because we use an array to store the reversed string
+// time complexity O(n) because we loop through the string once and use reverse method
+// space complexity O(n) because we use an array to store the reversed string and use split method to convert the string to an array
 
-//! test
-console.log(revString("ziad"));
+// test
+console.log("Rev2:", revString("ziad"));
+
+// ------------------------------
+
+// using spread operator and reverse method and join method
+const reverse = (str) => [...str].reverse().join("");
+// time complexity O(n) because we loop through the string once and use reverse method
+// space complexity O(n) because we use an array to store the reversed string and use spread operator to convert the string to an array
+
+// test
+console.log("Rev3:", reverse("ziad ahmed"));
+
+// ------------------------------
