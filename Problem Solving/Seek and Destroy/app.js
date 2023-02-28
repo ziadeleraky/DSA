b46cbe14b88ad42ec49335a864081c5a0d4d3792
Ziad Eleraky <ziadeleraky1@gmail.com>
2023-03-01 00:46:00 +0200

    Refactor Seek and Destroy

--- Problem Solving/Seek and Destroy/app.js
+++ Problem Solving/Seek and Destroy/app.js
@@ -1,14 +1,28 @@
 const seekAndDestroy = (arr) => {
-  const args = Array.from(arguments);
-  console.log(args); // [ [ 2, 3, 4, 6, 6, 'hello' ], 2, 6 ]
-
-  const filterArr = (arr) => {
-    // Return true if not in array
-    return args.indexOf(arr) === -1;
-  };
-
-  return arr.filter(filterArr);
+  const args = Array.from(arguments); // time-complexity: O(n) because we are looping through the arguments object to create an array
+  const destroy = args[0].filter((item) => !args.includes(item)); // time-complexity: O(n^2) because we are looping through the array and then looping through the arguments array to check if the item is in the arguments array
+  return destroy;
 };
 
-seekAndDestroy([2, 3, 4, 6, 6, "hello"], 2, 6);
-// console.log(seekAndDestroy([2, 3, 4, 6, 6, "hello"], 2, 6)); // [3, 4, "hello"]
+// time complexity: O(n^2)
+// space complexity: O(n) 
+
+console.log(seekAndDestroy([2, 3, 4, 6, 6, "hello"], 2, 6)); // [3, 4, "hello"]
+console.log(seekAndDestroy([2, 3, 4, 6, 6, "hello"], 2, 6, 4)); // [3, "hello"]
+console.log(seekAndDestroy([2, 3, 4, 6, 6, "hello"], 2, 6, 4, 3)); // ["hello"]
+
+// ========================================================
+
+// refactored code using Set
+const seekAndDestroy1 = (arr) => {
+  // reduce the time complexity of the arguments array by using Set to create a new array with unique values
+  const array = new Set(arr);
+  // arguments is not an array, so we need to convert it to an array
+  const args = Array.from(arguments);
+  // console.log(array); // Set(6) {2, 3, 4, 6, "hello", 5}
+  // filter the array and return the items that are not in the Set
+  const newArr = args[0].filter(item => !array.has(item));
+  return newArr;
+};
+
+console.log(seekAndDestroy1([2, 3, 4, 6, 6, "hello"], 2, 6)); // [3, 4, "hello"]
